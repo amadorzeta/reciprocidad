b791e208236dfb9256d9da41448bf64459dfc2ab
amador <joseamadorzeta@gmail.com>
2022-12-30 21:02:00 -0500

    Added styles + smoother input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,15 @@ import Head from "next/head";
 import React from "react";
 
 const Home: NextPage = () => {
-  const [measuredTime, setMeasuredTime] = React.useState<number>(0);
   const [calculatedTime, setCalculatedTime] = React.useState<number>(0);
 
   const calculateTime = (time: number) => {
     return Math.round(Math.pow(time, 1.2));
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setCalculatedTime(calculateTime(measuredTime));
-  };
-
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value.replace(/\D/g, "");
-    setMeasuredTime(+input);
+    setCalculatedTime(calculateTime(+input));
   };
 
   return (
@@ -30,29 +24,27 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-50 via-slate-700 to-slate-900">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">Reciprocidad</span>
+          <h1 className="text-5xl font-bold tracking-tight text-slate-900 sm:text-[5rem]">
+            <span>Reciprocidad</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">
-                Ingrese el tiempo calculado por el fotometro
-              </h3>
-              <form onSubmit={handleSubmit}>
-                <input
-                  className="w-full border-none bg-zinc-100 text-6xl text-slate-900"
-                  type="text"
-                  value={measuredTime}
-                  onChange={handleChange}
-                />
-              </form>
+
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-slate-800 p-4 text-slate-50">
+            <h3 className="text-2xl font-bold">
+              tiempo de exposicion en segundos
+            </h3>
+            <div className="w-full rounded-xl border-none bg-slate-50 p-4 text-6xl text-slate-900">
+              <input
+                className="w-full bg-inherit"
+                type="text"
+                onChange={handleChange}
+              />
             </div>
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">
-                + calculo de falla de reciprocidad
-              </h3>
+          </div>
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-slate-800 p-4 text-slate-50">
+            <h3 className="text-2xl font-bold">+ calculo de reciprocidad</h3>
+            <div className="w-full rounded-xl border-none bg-slate-50 p-4 text-6xl text-slate-900">
               <span>{calculatedTime}</span>
             </div>
           </div>
